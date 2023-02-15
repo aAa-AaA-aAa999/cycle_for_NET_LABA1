84358283b0c6bd78abf3135b15ab6a04bb6c0b5a
7x7x49.mrr <98632330+kitty7x7x49@users.noreply.github.com>
2023-02-15 21:16:35 +0300

    C#
    C#

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(" Введите n");
+            Console.ForegroundColor = ConsoleColor.White;
+            double n = Convert.ToDouble(Console.ReadLine());
+            
+            while (n < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" n должен быть положительным");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(" Введите x");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            double answer = 0;
+            for (int i = 0; i < n + 1; i++)
+            {
+                int fact = 1;
+                if (i != 0)
+                {
+                    for (int j = 2; j < i * 2 + 1; j++)
+                    {
+                        fact += j;
+                    }
+                }
+                answer += (Math.Pow(-1, i) * Math.Pow(x, 2 * i)) / fact;
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($" Ответ: {answer}");
+            Console.ReadKey();
+        }
+    }
+}
